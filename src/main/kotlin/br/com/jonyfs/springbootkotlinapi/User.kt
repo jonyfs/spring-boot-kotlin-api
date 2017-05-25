19d176f0c34bd9ed5adbc2e7a3e1a3a7da637a5d
Jony Santos <jony.santos@bematech.com.br>
2017-05-25 19:09:38 -0300

    initial commit

--- src/main/kotlin/br/com/jonyfs/springbootkotlinapi/User.kt
+++ src/main/kotlin/br/com/jonyfs/springbootkotlinapi/User.kt
@@ -1,6 +1,7 @@
 package br.com.jonyfs.springbootkotlinapi.user
 
 import br.com.jonyfs.springbootkotlinapi.role.Role
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.hibernate.validator.constraints.NotEmpty
 import java.io.Serializable
 import javax.persistence.*
@@ -25,6 +26,7 @@ class User : Serializable {
     @NotEmpty
     var email: String? = null
 
+    @JsonIgnore
     @NotEmpty
     var password: String? = null
 
